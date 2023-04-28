4a3335adb76a22b1bb2cf4004a9a1f908b7f4641
Kenneth Newall <kennethsean.newall@arvato.com>
2023-04-28 13:44:05 +0200

    added logging

---
+++ src/Infrastructure/Installers/SerilogInstaller.cs
@@ -0,0 +1,20 @@
+﻿using Application.Installers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Infrastructure.Installers;
+
+public class SerilogInstaller : IInstaller
+{
+    public void Install(IServiceCollection services, IConfiguration config)
+    {
+        services.AddLogging(
+            loggingBuilder =>
+            {
+                var logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
+
+                loggingBuilder.AddSerilog(logger, true);
+            });
+    }
+}
