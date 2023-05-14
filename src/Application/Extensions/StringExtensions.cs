81629385f34fa839caccac5c40eeddf67f4de302
Kenneth Newall <kennethsean.newall@arvato.com>
2023-05-14 18:26:27 +0200

    added proof of work test

--- src/Application/Extensions/StringExtensions.cs
+++ src/Application/Extensions/StringExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace Application.Extensions;
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Application.Extensions;
 
 public static class StringExtensions
 {
@@ -7,4 +10,16 @@ public static class StringExtensions
 
     public static string JoinToString<T, T2>(this IEnumerable<KeyValuePair<T, T2>> enumerable, string separator = ",") =>
         string.Join(separator, enumerable.Select(e => $"{e.Key}={e.Value}"));
+    
+    public static string ComputeSha256Hash(this string rawData)
+    {
+        var buffer = Encoding.UTF8.GetBytes(rawData);
+        var bytes = SHA256.HashData(buffer);
+        var builder = new StringBuilder();
+
+        foreach (var @byte in bytes)
+            builder.Append(@byte.ToString("x2"));
+
+        return builder.ToString();
+    }
 }
---
+++ tests/Infrastructure.Integration/ProofOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Application.Extensions;
+using Xunit;
+
+namespace Infrastructure.Integration;
+
+public class ProofOfWork
+{
+    [Fact]
+    public void FindBlock()
+    {
+        var hash = "";
+        var hashNo = 0;
+        var interactionId = Guid.NewGuid().ToString();
+
+        while (!hash.StartsWith("0000"))
+            hash = $"{interactionId}_{++hashNo}".ComputeSha256Hash();
+        
+        Assert.Equal(hash, $"{interactionId}_{hashNo}".ComputeSha256Hash());
+    }
+}
