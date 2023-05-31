194d1e1933d80df0d6e7063d237fd81003aa8ac6
Kenneth Newall <kennethsean.newall@arvato.com>
2023-05-31 19:55:07 +0200

    "ddos client"

---
+++ src/Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Net.Http.Json;
+
+var stop = TimeSpan.FromSeconds(60);
+var i = 0;
+var task = Task.Run(async () =>
+{
+    var stopwatch = Stopwatch.StartNew();
+    var http = new HttpClient
+    {
+        BaseAddress = new Uri("https://api.btcis.me/v1/")
+    };
+
+    while (stopwatch.Elapsed < stop)
+    {
+        i++;
+        await http.GetFromJsonAsync<BlockDto[]>("blocks");
+    }
+});
+var tasks = new[] { task, task, task, task, task, task, task, task, task };
+
+await Parallel.ForEachAsync(tasks, async (innerTask, _) => await innerTask);
+
+Console.WriteLine($"Executed {i} requests in {stop}");
+
+public class BlockDto
+{
+    public string Id { get; init; } = "";
+
+    public long Height { get; init; }
+
+    public DateTime TimeStamp { get; init; }
+
+    public ulong Nonce { get; init; }
+
+    public double Difficulty { get; init; }
+
+    public double Probability { get; init; }
+}
