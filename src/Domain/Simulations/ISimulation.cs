efa42df59ff53a892a2754d098305cb7b90be1b6
Kenneth Newall <kennethsean.newall@arvato.com>
2023-06-02 13:04:47 +0200

    added missing property to ISimulation

--- src/Domain/Simulations/ISimulation.cs
+++ src/Domain/Simulations/ISimulation.cs
@@ -1,6 +1,10 @@
-﻿namespace Domain.Simulations;
+﻿using System.Text.Json;
+
+namespace Domain.Simulations;
 
 public interface ISimulation
 {
     string SimulationType { get; }
+    
+    JsonElement? Result { get; set; }
 }
