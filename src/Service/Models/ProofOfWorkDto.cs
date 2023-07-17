f701dfb15feb51385d7e0a9bd452a8235aebc89f
Kenneth Newall <kennethsean.newall@arvato.com>
2023-07-17 16:51:25 +0200

    added SecondsToSkipValidBlocks

--- src/Service/Models/ProofOfWorkDto.cs
+++ src/Service/Models/ProofOfWorkDto.cs
@@ -1,14 +1,16 @@
 ﻿namespace Service.Models;
 
 public record ProofOfWorkDto
-{ 
+{
     public int SecondsUntilBlock { get; init; }
 
-    public double TotalHashRate { get; set; } 
-    
+    public int SecondsToSkipValidBlocks { get; init; }
+
+    public double TotalHashRate { get; set; }
+
     public double Difficulty { get; set; }
-    
-    public double Expected { get; set; } 
-    
+
+    public double Expected { get; set; }
+
     public string Threshold { get; set; } = "";
 }
