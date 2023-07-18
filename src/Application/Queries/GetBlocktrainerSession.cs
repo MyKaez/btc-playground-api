23c4d4232bc96264bf7bf62d1925d4eed17ca4c4
Kenneth Newall <kennethsean.newall@arvato.com>
2023-07-18 13:25:38 +0200

    added static blocktrainer session

---
+++ src/Application/Queries/GetBlocktrainerSession.cs
@@ -0,0 +1,29 @@
+﻿using Application.Handlers;
+using Application.Models;
+using Application.Services;
+using Domain.Models;
+
+namespace Application.Queries;
+
+public static class GetBlocktrainerSession
+{
+    public record Query : Request<Session>;
+    
+    public class Handler : RequestHandler<Query, Session>
+    {
+        private readonly ISessionService _sessionService;
+
+        public Handler(ISessionService sessionService)
+        {
+            _sessionService = sessionService;
+        }
+
+        public override async Task<Result<Session>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var sessions = await _sessionService.GetAll(cancellationToken);
+            var session = sessions.FirstOrDefault(s => s.Name == "Blocktrainer");
+
+            return session ?? NotFound();
+        }
+    }
+}
--- src/Service/BackgroundJobs/CreateBlocktrainerSession.cs
+++ src/Service/BackgroundJobs/CreateBlocktrainerSession.cs
@@ -19,16 +19,16 @@ public class CreateBlocktrainerSession : BackgroundService
             Extensions.SessionExtensions.BlocktrainerSessionName, JsonDocument.Parse("{}").RootElement, stoppingToken);
 
         if (session is null)
-            throw new NotSupportedException($"Cannot create Blocktrainer session");
+            throw new NotSupportedException("Cannot create Blocktrainer session");
 
         while (!stoppingToken.IsCancellationRequested)
         {
             var blockTrainerSession = await _sessionService.GetById(session.Id, stoppingToken);
 
             if (blockTrainerSession is null)
-                throw new NotSupportedException($"Cannot find Blocktrainer session");
+                throw new NotSupportedException("Cannot find Blocktrainer session");
 
-            var update = new SessionUpdate { SessionId = session.Id, Configuration = blockTrainerSession.Configuration.Value };
+            var update = new SessionUpdate { SessionId = session.Id, Configuration = blockTrainerSession.Configuration!.Value };
 
             await _sessionService.UpdateSession(update, stoppingToken);
             await Task.Delay(60_000, stoppingToken);
