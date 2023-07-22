a509e7af4331fa9b23f13718e9b22f3ccd2b3ffb
Kenneth Newall <kennethsean.newall@arvato.com>
2023-07-22 13:53:21 +0200

    added Update modes

---
+++ src/Client/Models/Update.cs
@@ -0,0 +1,16 @@
+﻿namespace Client.Models;
+
+public class Update
+{
+    public Guid? Id { get; set; }
+
+    public string? Status { get; set; }
+
+    public string? Configuration { get; set; }
+    
+    public DateTime? StartTime { get; set; }
+    
+    public DateTime? EndTime { get; set; }
+    
+    public bool? Urgent { get; set; }
+}
